8647faeb35788b1d7d852ad7bcb0a408d0050916
Maksim Khomenkov <maksimhomenkov@yandex.ru>
2023-10-19 20:00:28 +0300

    доработанная версия (исправлен порядок класса ViewController)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,61 +7,62 @@
 
 import UIKit
 
-var dateString:  String!
-func getDate(){
-    let dateFormatter = DateFormatter()
 
-    dateFormatter.dateFormat = "dd-MM-YYY hh:mm:ss"
-    return dateString = dateFormatter.string(from: Date())
-}
+
 
 class ViewController: UIViewController {
-    public var labelCount : Int = 0
-    public func newString(){
-        changeHistory.scrollRangeToVisible(NSMakeRange(changeHistory.text.count - 1, 0))
+    
+    @IBOutlet private var labelText: UILabel!
+    
+    @IBOutlet private var buttonPlus: UIButton!
+    
+    @IBOutlet private var buttonMinus: UIButton!
+    
+    @IBOutlet private var clearAllButton: UIButton!
+    
+    @IBOutlet private var changeHistoryTextView: UITextView!
+    
+    private var labelCount : Int = 0
+    private var dateString = ""
+    
+    private func getDate(){
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateFormat = "dd-MM-YYY hh:mm:ss"
+        return dateString = dateFormatter.string(from: Date())
     }
-    
-    @IBOutlet weak var labelText: UILabel!
-    
-    @IBOutlet weak var buttonPlus: UIButton!
-    
-    @IBOutlet weak var buttonMinus: UIButton!
-    
-    @IBOutlet weak var clearAll: UIButton!
-    
-    @IBOutlet weak var changeHistory: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        labelText.text = "Значение счетчика: 0"
-        changeHistory.text = "История изменений: "
+    private func newString(){
+        changeHistoryTextView.scrollRangeToVisible(NSMakeRange(changeHistoryTextView.text.count - 1, 0))
     }
 
-    @IBAction func pressClearAll(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labelText.text = "Значение счетчика: 0"
+        changeHistoryTextView.text = "История изменений: "
+    }
+
+    @IBAction private func pressClearAllButton(_ sender: Any) {
         labelCount = 0
         labelText.text = "Значение счетчика: \(labelCount)"
         getDate()
-        changeHistory.text.append("\n[\(dateString!)]: \nзначение сброшено")
+        changeHistoryTextView.text.append("\n[\(dateString)]: \nзначение сброшено")
         newString()
     }
-    
-    @IBAction func pressButtonPlus(_ sender: Any) {
+    @IBAction private func pressButtonPlus(_ sender: Any) {
         labelCount += 1
         labelText.text = "Значение счетчика: \(labelCount)"
         getDate()
-        changeHistory.text.append("\n[\(dateString!)]: \nзначение изменено  на +1")
+        changeHistoryTextView.text.append("\n[\(dateString)]: \nзначение изменено  на +1")
         newString()
     }
-    
-    @IBAction func pressButtonMinus(_ sender: Any) {
+    @IBAction private func pressButtonMinus(_ sender: Any) {
         getDate()
         if labelCount > 0 {
             labelCount -= 1
-            changeHistory.text.append("\n[\(dateString!)]: \nзначение изменено на -1")
+            changeHistoryTextView.text.append("\n[\(dateString)]: \nзначение изменено на -1")
             labelText.text = "Значение счетчика: \(labelCount)"
         } else {
-            changeHistory.text.append("\n[\(dateString!)]: \nпопытка уменьшить значение счётчика ниже 0")
+            changeHistoryTextView.text.append("\n[\(dateString)]: \nпопытка уменьшить значение счётчика ниже 0")
         }
         newString()
     }
